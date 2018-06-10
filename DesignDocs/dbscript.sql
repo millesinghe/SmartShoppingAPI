2051e7764504bacca3b4bbbcea759fa58e0a43ac
Milinda Bandara <dev.milinda@gmail.com>
2018-06-10 19:44:20 +0530

    commit with db script changes

--- DesignDocs/dbscript.sql
+++ DesignDocs/dbscript.sql
@@ -5,19 +5,21 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema SmartShop
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema SmartShop
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `smartshop` DEFAULT CHARACTER SET utf8 ;
+USE `SmartShop` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`shop`
+-- Table `SmartShop`.`shop`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`shop` (
+DROP TABLE IF EXISTS `smartshop`.`shop` ;
+
+CREATE TABLE IF NOT EXISTS `smartshop`.`shop` (
   `idshop` INT NOT NULL,
   `shopName` VARCHAR(45) NOT NULL,
   `shopBranch` VARCHAR(45) NULL,
@@ -28,9 +30,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`stock`
+-- Table `SmartShop`.`stock`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`stock` (
+DROP TABLE IF EXISTS `smartshop`.`stock` ;
+
+CREATE TABLE IF NOT EXISTS `smartshop`.`stock` (
   `iditem` INT NOT NULL,
   `itemName` VARCHAR(45) NOT NULL,
   `itemDesc` VARCHAR(45) NULL,
@@ -41,19 +45,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`stock` (
   `availableAmount` DOUBLE NULL,
   `shop_idshop` INT NOT NULL,
   PRIMARY KEY (`iditem`, `shop_idshop`),
-  INDEX `fk_Stock_shop_idx` (`shop_idshop` ASC),
-  CONSTRAINT `fk_Stock_shop`
-    FOREIGN KEY (`shop_idshop`)
-    REFERENCES `mydb`.`shop` (`idshop`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  INDEX `fk_Stock_shop_idx` (`shop_idshop` ASC))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`order`
+-- Table `SmartShop`.`order`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`order` (
+DROP TABLE IF EXISTS `smartshop`.`order` ;
+
+CREATE TABLE IF NOT EXISTS `SmartShop`.`order` (
   `idOrder` INT NOT NULL AUTO_INCREMENT,
   `CustomerId` VARCHAR(45) NOT NULL,
   `TableId` VARCHAR(45) NOT NULL,
@@ -62,9 +63,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`employee`
+-- Table `SmartShop`.`employee`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`employee` (
+DROP TABLE IF EXISTS `smartshop`.`employee` ;
+
+CREATE TABLE IF NOT EXISTS `SmartShop`.`employee` (
   `idemployee` VARCHAR(50) NOT NULL,
   `employeeName` VARCHAR(255) NOT NULL,
   `employeeTele` VARCHAR(45) NOT NULL,
@@ -72,19 +75,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`employee` (
   `employeePosition` VARCHAR(45) NOT NULL,
   `shop_idshop` INT NOT NULL,
   PRIMARY KEY (`idemployee`, `shop_idshop`),
-  INDEX `fk_employee_shop1_idx` (`shop_idshop` ASC),
-  CONSTRAINT `fk_employee_shop1`
-    FOREIGN KEY (`shop_idshop`)
-    REFERENCES `mydb`.`shop` (`idshop`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  INDEX `fk_employee_shop1_idx` (`shop_idshop` ASC))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Order_has_Stock`
+-- Table `SmartShop`.`Order_has_Stock`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Order_has_Stock` (
+DROP TABLE IF EXISTS `smartshop`.`Order_has_Stock` ;
+
+CREATE TABLE IF NOT EXISTS `smartshop`.`Order_has_Stock` (
   `Order_idOrder` INT NOT NULL,
   `Stock_iditem` INT NOT NULL,
   `ReadyStatus` TINYINT(1) NOT NULL,
@@ -92,22 +92,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Order_has_Stock` (
   PRIMARY KEY (`Order_idOrder`, `Stock_iditem`, `employee_idemployee`),
   INDEX `fk_Order_has_Stock_Stock1_idx` (`Stock_iditem` ASC),
   INDEX `fk_Order_has_Stock_Order1_idx` (`Order_idOrder` ASC),
-  INDEX `fk_Order_has_Stock_employee1_idx` (`employee_idemployee` ASC),
-  CONSTRAINT `fk_Order_has_Stock_Order1`
-    FOREIGN KEY (`Order_idOrder`)
-    REFERENCES `mydb`.`order` (`idOrder`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Order_has_Stock_Stock1`
-    FOREIGN KEY (`Stock_iditem`)
-    REFERENCES `mydb`.`stock` (`iditem`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Order_has_Stock_employee1`
-    FOREIGN KEY (`employee_idemployee`)
-    REFERENCES `mydb`.`employee` (`idemployee`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  INDEX `fk_Order_has_Stock_employee1_idx` (`employee_idemployee` ASC))
 ENGINE = InnoDB;
 
 
